d984ca7ed50166722be5921c5b6ba29bec1ca6de
JiaLei <sfw123817@gmail.com>
2016-10-11 23:48:27 +0800

    invoke this function

---
+++ src/jialei/javacamp/basicknowledge/InitAndCleanUp/Flower.java
@@ -0,0 +1,24 @@
+package jialei.javacamp.basicknowledge.InitAndCleanUp;
+
+/**
+ * Created by JiaLei on 2016/10/11.
+ */
+public class Flower {
+    public Flower() {
+
+    }
+
+    public Flower(int a) {
+        int c = 2;
+        //this();
+    }
+
+    public Flower(int a, int b) {
+        this(a);
+        //this();
+    }
+
+    public void open() {
+        //this();
+    }
+}
