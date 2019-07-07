d44630c7552528552ac4ac85c8d5d9b5b042983b
JiaLei <sfw123817@gmail.com>
2019-07-08 00:35:41 +0800

    add more bean

---
+++ SpringPractice/WireBeanAdv/src/main/java/com/jialei/externals/BlankDisc.java
@@ -0,0 +1,22 @@
+package com.jialei.externals;
+
+/**
+ * Created by JiaLei on 2019/7/07.
+ */
+public class BlankDisc {
+    private final String title;
+    private final String artist;
+
+    public BlankDisc(String title, String artist) {
+        this.title = title;
+        this.artist = artist;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+}
---
+++ SpringPractice/WireBeanAdv/src/test/java/com/jialei/externals/EnvironmentConfig.java
@@ -0,0 +1,22 @@
+package com.jialei.externals;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
+@Configuration
+@PropertySource("class:/properties")
+public class EnvironmentConfig {
+    @Autowired
+    Environment env;
+
+    @Bean
+    public BlankDisc blankDisc() {
+        return new BlankDisc(
+                env.getProperty("disc.title"),
+                env.getProperty("disc.artist"));
+    }
+}
