4435e0f3b3b6ca6301e0ecade4ef79a81fd4a595
JiaLei <sfw123817@gmail.com>
2019-08-04 08:47:58 +0800

    add spring mvc code

---
+++ SpringPractice/SpringMVCBasic/src/test/java/jialei/com/spitter/HomeControllerTest.java
@@ -0,0 +1,19 @@
+package jialei.com.spitter;
+
+import jialei.com.spitter.web.HomeController;
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+public class HomeControllerTest {
+    @Test
+    public void testHomePage() throws Exception {
+        HomeController controller = new HomeController();
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+        mockMvc.perform(MockMvcRequestBuilders.get("/")).andExpect(MockMvcResultMatchers.view().name("home"));
+    }
+
+}
