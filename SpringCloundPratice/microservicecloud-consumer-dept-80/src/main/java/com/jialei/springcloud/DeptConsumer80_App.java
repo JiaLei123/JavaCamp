8fa2351c632f8312c3bdd1b3ed991949de692e87
JiaLei <sfw123817@gmail.com>
2019-08-11 00:34:49 +0800

    服务注册

--- SpringCloundPratice/microservicecloud-consumer-dept-80/src/main/java/com/jialei/springcloud/DeptConsumer80_App.java
+++ SpringCloundPratice/microservicecloud-consumer-dept-80/src/main/java/com/jialei/springcloud/DeptConsumer80_App.java
@@ -2,12 +2,10 @@ package com.jialei.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class DeptProvider8001_App {
+public class DeptConsumer80_App {
     public static void main( String[] args ) {
-        SpringApplication.run(DeptProvider8001_App.class, args);
+        SpringApplication.run(DeptConsumer80_App.class, args);
     }
 }
