985205d52b6cb0f24056b8cfcc29005be731fe23
日游 <jialei.jl@antfin.com>
2019-08-16 14:17:07 +0800

    加入Kotlin项目

--- JavaBasic/src/main/java/com/jialei/ThreadDem/InvokeAllDemo.java
+++ JavaBasic/src/main/java/com/jialei/ThreadDem/InvokeAllDemo.java
@@ -37,5 +37,7 @@ public class InvokeAllDemo {
         } catch (ExecutionException e) {
             e.printStackTrace();
         }
+
+        executor.shutdown();
     }
 }
--- KotlinBasic/src/com/jialei/FirstDeom.kt
+++
@@ -1,6 +0,0 @@
-package com.jialei
-
-fun main(args: Array<String>) {
-    var name = "three"
-    System.out.println(name)
-}
