02938aacfe2713f04593821839653ab07c358add
JiaLei <sfw123817@gmail.com>
2019-09-13 11:38:48 +0800

    add spring boot

---
+++ JavaThread/ShowCompletableFuture/src/test/java/com/jialei/BasicExampleTest.java
@@ -0,0 +1,20 @@
+package com.jialei;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple BasicExample.
+ */
+public class BasicExampleTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
