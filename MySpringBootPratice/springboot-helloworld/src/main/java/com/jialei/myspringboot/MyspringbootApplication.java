ed4fa3e9ab00c72948c3f2311efd343e9343ca83
日游 <jialei.jl@antfin.com>
2019-09-23 11:37:15 +0800

    打开debug = true

--- MySpringBootPratice/springboot-helloworld/src/main/java/com/jialei/myspringboot/MyspringbootApplication.java
+++ MySpringBootPratice/springboot-helloworld/src/main/java/com/jialei/myspringboot/MyspringbootApplication.java
@@ -4,12 +4,17 @@ import com.jialei.myspringboot.bean.Person;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @SpringBootApplication
 public class MyspringbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyspringbootApplication.class, args);
+		Logger logger = LoggerFactory.getLogger(MyspringbootApplication.class);
+		logger.info("run application");
 	}
 
 
