6b259526e337e4cc4aed40d351197f141e49e531
日游 <jialei.jl@antfin.com>
2019-10-16 11:13:39 +0800

    登陆和连接器

--- MySpringBootPratice/springboot-helloworld/src/main/java/com/jialei/myspringboot/config/MyMvcConfig.java
+++ MySpringBootPratice/springboot-helloworld/src/main/java/com/jialei/myspringboot/config/MyMvcConfig.java
@@ -1,8 +1,10 @@
 package com.jialei.myspringboot.config;
 
+import com.jialei.myspringboot.component.LoginHandlerInterceptor;
 import com.jialei.myspringboot.component.MyLocaleResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.LocaleResolver;
@@ -11,12 +13,20 @@ import org.springframework.web.servlet.LocaleResolver;
 @Configuration
 public class MyMvcConfig extends WebMvcConfigurerAdapter {
     @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        //super.addInterceptors(registry);
+        registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/**");
+    }
+
+    @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/jialei").setViewName("sucess");
-        registry.addViewController("/main").setViewName("login");
+        registry.addViewController("/main").setViewName("list");
         registry.addViewController("/login").setViewName("login");
         super.addViewControllers(registry);
     }
+
+
 
     @Bean
     public LocaleResolver localeResolver(){
