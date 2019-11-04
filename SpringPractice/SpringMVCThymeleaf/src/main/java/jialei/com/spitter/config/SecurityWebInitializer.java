def59197c747a7d6cc9583f30492760d6739493d
日游 <jialei.jl@antfin.com>
2019-11-04 10:34:56 +0800

    添加拦截请求

--- SpringPractice/SpringMVCThymeleaf/src/main/java/jialei/com/spitter/config/SecurityWebInitializer.java
+++ SpringPractice/SpringMVCThymeleaf/src/main/java/jialei/com/spitter/config/SecurityWebInitializer.java
@@ -3,19 +3,23 @@ package jialei.com.spitter.config;
 import org.omg.CORBA.PRIVATE_MEMBER;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
 import javax.sql.DataSource;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebMvcSecurity
 public class SecurityWebInitializer extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
@@ -29,5 +33,16 @@ public class SecurityWebInitializer extends WebSecurityConfigurerAdapter {
 //        .passwordEncoder(new StandardPasswordEncoder("53cr3t"));
 //        auth.userDetailsService();
 
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").authenticated()
+                .antMatchers("/thy/spittles").authenticated()
+                .antMatchers("/spittles").authenticated()
+                .antMatchers("/spitter/me").authenticated()
+                .antMatchers(HttpMethod.POST, "/spittles").authenticated()
+                .anyRequest().authenticated();
     }
 }
