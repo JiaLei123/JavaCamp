48ed49cfb8906c9a820ec5be1212f27054a7105c
JiaLei <sfw123817@gmail.com>
2019-11-16 22:10:04 +0800

    实现spring mvc的数据源

--- SpringPractice/SpringMVCThymeleaf/src/main/java/jialei/com/spitter/config/DataConfig.java
+++ SpringPractice/SpringMVCThymeleaf/src/main/java/jialei/com/spitter/config/DataConfig.java
@@ -1,7 +1,9 @@
 package jialei.com.spitter.config;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
@@ -10,12 +12,24 @@ import javax.sql.DataSource;
 
 @Configuration
 public class DataConfig {
+    @Profile("test")
     @Bean
     public DataSource dataSource(){
         return new EmbeddedDatabaseBuilder()
                 .setType(EmbeddedDatabaseType.H2)
                 .addScript("schema.sql")
                 .build();
+    }
+
+    @Profile("dev")
+    @Bean
+    public DataSource druid(){
+        DruidDataSource druid = new DruidDataSource();
+        druid.setUsername("root");
+        druid.setPassword("123123");
+        druid.setDriverClassName("com.mysql.jdbc.Driver");
+        druid.setUrl("jdbc:mysql://localhost:3306/Spitter");
+        return druid;
     }
 
     @Bean
