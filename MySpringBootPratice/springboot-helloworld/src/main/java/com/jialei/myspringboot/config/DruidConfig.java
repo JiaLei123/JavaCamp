e9063f7096a50299e66fc381c07df7980f0c842f
日游 <jialei.jl@antfin.com>
2019-11-18 11:38:53 +0800

    durid 数据访问监控

--- MySpringBootPratice/springboot-helloworld/src/main/java/com/jialei/myspringboot/config/DruidConfig.java
+++ MySpringBootPratice/springboot-helloworld/src/main/java/com/jialei/myspringboot/config/DruidConfig.java
@@ -5,12 +5,15 @@ package com.jialei.myspringboot.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -36,6 +39,19 @@ public class DruidConfig {
         initParam.put("loginPassword", "123456");
         initParam.put("allow", ""); //默认是允许所有的访问
         bean.setInitParameters(initParam);
+        return bean;
+    }
+
+    @Bean
+    public FilterRegistrationBean webStatFilter(){
+        FilterRegistrationBean bean = new FilterRegistrationBean();
+        bean.setFilter(new WebStatFilter());
+
+        Map<String, String> initParam = new HashMap<>();
+        initParam.put("exclusions", "*.js, *.css, /druid");
+
+        bean.setInitParameters(initParam);
+        bean.setUrlPatterns(Arrays.asList("/*"));
         return bean;
     }
 }
