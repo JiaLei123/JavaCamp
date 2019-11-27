efa715e7657d93618475146fc9a4a9e67967e634
日游 <jialei.jl@antfin.com>
2019-11-27 14:24:54 +0800

    初次添加Hibernate

--- SpringPractice/SpringMVCThymeleaf/src/main/java/jialei/com/spitter/config/DataConfig.java
+++ SpringPractice/SpringMVCThymeleaf/src/main/java/jialei/com/spitter/config/DataConfig.java
@@ -1,14 +1,18 @@
 package jialei.com.spitter.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import org.hibernate.SessionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
 
 import javax.sql.DataSource;
+import java.io.IOException;
+import java.util.Properties;
 
 @Configuration
 public class DataConfig {
@@ -35,5 +39,18 @@ public class DataConfig {
     @Bean
     public JdbcTemplate jdbcTemplate(DataSource dataSource){
         return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public SessionFactory sessionFactoryBean(DataSource dataSource) throws IOException {
+        LocalSessionFactoryBean lsfb = new LocalSessionFactoryBean();
+        lsfb.setDataSource(dataSource);
+        lsfb.setPackagesToScan("jialei.com.spitter.model");
+        Properties props = new Properties();
+        props.setProperty("dialect", "org.hibernate.dialeet.H2Dialect");
+        lsfb.setHibernateProperties(props);
+        lsfb.afterPropertiesSet();
+        SessionFactory sessionFactory = lsfb.getObject();
+        return sessionFactory;
     }
 }
