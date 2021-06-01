3a09b79a1505e7c33a8c56def4ea2412f6be5420
日游 <jialei.jl@antfin.com>
2021-06-01 10:29:59 +0800

    支持cloneable

---
+++ JavaBasic/src/main/java/com/jialei/effictivejava/DemoCloneObject.java
@@ -0,0 +1,34 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2021 All Rights Reserved.
+ */
+package com.jialei.effictivejava;
+
+/**
+ * @author jialei
+ * @version : CloneObject.java, v 0.1 2021年05月31日 11:57 下午 jialei Exp $
+ */
+public class DemoCloneObject implements Cloneable{
+
+    private final String name;
+
+    //@Override
+    //protected Object clone() {
+    //    return super.clone();
+    //}
+
+    public DemoCloneObject(String name){
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        DemoCloneObject testOjb = null;
+        DemoCloneObject obj = new DemoCloneObject("Test");
+        try {
+            testOjb = (DemoCloneObject)obj.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(testOjb);
+    }
+}
